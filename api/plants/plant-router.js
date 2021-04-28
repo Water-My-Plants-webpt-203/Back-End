d8ab6586f24fc6fc950702bdd63cbfe15b0ebdd3
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-04-27 19:12:28 -0700

    Added Image feature

--- api/plants/plant-router.js
+++ api/plants/plant-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const restricted = require('../auth/auth-middleware.js');
 const plantModel = require('../plants/plant-model.js');
+const path = require('path');
+const fs = require('fs');
 
 router.get('/:plant_id', restricted,  async (req, res) => {
     try{
@@ -24,8 +26,43 @@ router.get('/:user_id/plants', restricted, async (req, res) => {
 
 router.post('/:user_id/plants', restricted, async (req, res) => {
     try{
-        await plantModel.insert(req.params.user_id, req.body);
-        res.status(201).json({ message: "Plant created successfully."});
+        const user_id = req.params.user_id;
+        if(req.files){
+            const file = req.files.plantImg;
+            const fileExt = file.name.split('.').pop();
+            console.log(fileExt);
+            if(fileExt.toLowerCase() == "jpeg" || fileExt.toLowerCase() == "jpg"){
+                let dirPath = path.dirname(require.main.filename) + '\\api\\images\\' +user_id;
+                let imageName = file.name.substr(0, file.name.lastIndexOf('.')) + Date.now()+ '.' + fileExt;
+                let imageLocation = dirPath + '\\' + imageName;
+                if(fs.existsSync(dirPath)){
+                    if(file){
+                        file.mv(imageLocation);
+                    }
+                }
+                else{
+                    fs.mkdir(dirPath,function(error){
+                        if(error){
+                            console.log(error);
+                        }
+                    });
+                    if(file){
+                        file.mv(imageLocation);
+                    }
+                }
+                req.body.image = user_id + '/' + imageName;
+                await plantModel.insert(req.params.user_id, req.body);
+                res.status(201).json({ message: "Plant created successfully."});
+            }
+            else{
+                res.status(400).json({ message: "Only JPEG or JPG file allowed."});
+            }
+        }
+        else{
+            req.body.image = 'default/default.jpg';
+            await plantModel.insert(req.params.user_id, req.body);
+            res.status(201).json({ message: "Plant created successfully."});
+        }
     }
     catch(error){
         res.status(500).json({ message: error.message })
--- api/server.js
+++ api/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
 const authRouter = require('./auth/auth-router.js');
 const userRouter = require('./users/user-router.js');
 const plantRouter = require('./plants/plant-router.js');
 const server = express();
 
+server.use(fileUpload());
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use('/images', express.static(path.join(__dirname, '/images')));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', userRouter);
